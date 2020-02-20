ac8e0d0b5cfe3741005ef08d28cfe21275fbe5dc
suzie4831 <suzie4831@naver.com>
2020-02-20 18:17:16 +0900

    마이페이지 내 회원탈퇴 추가

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('login/', views.login, name="login"),
     path('logout/',views.logout, name="logout"),
+    path('mypage/',views.mypage,name="mypage"),
+    path('mem_delete/', views.mem_delete, name="mem_delete"),
 ]
